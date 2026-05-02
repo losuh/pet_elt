0ae277997f99ba2d4dca5bd2603db13346768dfa
losuh <mai.konovalov.daniil@gmail.com>
2026-05-02 19:57:26 +0300

    cleaning

--- ch_init/init.sql
+++
@@ -1,9 +0,0 @@
--- Сначала создаем базы
-CREATE DATABASE IF NOT EXISTS ext;
-CREATE DATABASE IF NOT EXISTS dwh;
-
--- Потом создаем таблицу, явно указывая базу данных
-CREATE TABLE IF NOT EXISTS ext.my_test_table (
-    id UInt64,
-    name String
-) ENGINE = MergeTree() ORDER BY id;
